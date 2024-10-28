4ed3c721f830f53fb5c859f78e34cacef529723a
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-28 12:11:58 -0400

    Add project files.

---
+++ IS3050-001 Fall 2024 Assignment 08/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Follow these steps, then delete the steps from the module:
+ * 0. All team members: meet to divide the assignment responsibilities:
+ *                      (A) Team Member #1 will program the entry point code, Program.cs.
+ *                      (B) The other team members will each program one class to model a real-world entity, per the assignment.
+ *                      
+ * 1. Team Member #1: fork and clone this VS Solution.
+ * 2. Team Member #1: add a class module for each of the classes the other team members will write (see Step 0, above)
+ * 3. Team Member #1: Commit and push the VS Solution. There will be multiple class modules in this new version: Program.cs and the mostly empty modules from Step 2, above.
+ * 4. Team Members #2... : Fork and clone the repo from Step 3, above. 
+ * 5. Team Members #2... : Modify the corresponding class module created in Step 2, above, per the assignment
+ * 6. Team Members #2... : Commit and push, then create a Pull Request
+ * 7. Team Member #1: Modify Program.cs as necessary (refer to the assignment), then commit and push
+ * 8. Team Member #1: Open the repo on GitHub.com, accept the pull requests from Step 6, above. There should be no merge conflicts if all team members stayed in their lanes.
+ * 9. All Team Members: Execute a Synch on your local VS Solution, verify that everything works as expected
+ * 10. Any team member: Submit the appropriate URL to Canvas for grading. 
+ * */
+// Document here per our requirements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS3050_001_Fall_2024_Assignment_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and instantiate the classes defined your project team
+        }
+    }
+}
