0e98860c6e4af438202801b350befffac234e3fc
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-30 17:10:41 -0400

    Local Class01 Test

---
+++ IS3050-001 Fall 2024 Assignment 08/LocalClass01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS3050_001_Fall_2024_Assignment_08
+{
+    internal class LocalClass01
+    {
+    }
+}
