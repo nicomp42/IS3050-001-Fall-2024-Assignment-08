c557b39c27995b56f7dd5bcaff5fee5a50912e1b
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-30 17:30:39 -0400

    Clean up

--- IS3050-001 Fall 2024 Assignment 08/Program.cs
+++ IS3050-001 Fall 2024 Assignment 08/Program.cs
@@ -4,15 +4,15 @@
  *                      (B) The other team members will each program one class to model a real-world entity, per the assignment.
  *                      
  * 1. Team Member #1: fork and clone this VS Solution. https://github.com/nicomp42/IS3050-001-Fall-2024-Assignment-08
- * 2. Team Member #1: add a class module for each of the classes the other team members will write (see Step 0, above)
- * 3. Team Member #1: Commit and push the VS Solution. There will be multiple class modules in this new version: Program.cs and the mostly empty modules from Step 2, above.
- * 4. Team Members #2... : Fork and clone the repo from Step 3, above. 
- * 5. Team Members #2... : Modify the corresponding class module created in Step 2, above, per the assignment
- * 6. Team Members #2... : Commit and push, then create a Pull Request
- * 7. Team Member #1: Modify Program.cs as necessary (refer to the assignment), then commit and push
- * 8. Team Member #1: Open the repo on GitHub.com, accept the pull requests from Step 6, above. There should be no merge conflicts if all team members stayed in their lanes.
- * 9. All Team Members: Execute a Synch on your local VS Solution, verify that everything works as expected
- * 10. Any team member: Submit the appropriate URL to Canvas for grading. 
+ * 2. Team Member #1: Add appropriate docuentation to Program.cs, commit and push the VS Solution to GitHub.
+ * 3. Team Members #2... : Fork and clone the repo from Step 2, above. 
+ * 4. Team Members #2... : Add your class module to the project and program it accordingly, per the assignment. Do not modify Program.cs.
+ * 5. Team Members #2... : From VS, "Commit All and Synch", then from GitHub, create a Pull Request
+ * 6. Team Member #1: Modify Program.cs as necessary (refer to the assignment), then from VS, "Commit All and Synch"
+ * 7. Team Member #1: Open the repo on GitHub.com, accept and merge all the pull requests from Step 5, above. There should be no merge conflicts if all team members stayed in their lanes.
+ * 8. All Team Members: Execute a Pull on your local VS Solution, verify that everything works as expected. If there's a problem, fix it, then from VS, "Commit All and Synch"
+ * 9. Any team member: Submit the appropriate URL to Canvas for grading. At this point all the repos in the team should have the same contents, 
+ *    but it you're unsure, use the repo from Team Member #1.
  * */
 // Document here per our requirements
 using System;
